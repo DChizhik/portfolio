dc187253ecfbe44c7881cbe58bdf857da4cae0b5
DChizhik <147550932+DChizhik@users.noreply.github.com>
2026-09-10 23:29:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -92,6 +92,8 @@
 	    animateIn: 'fadeIn',
 	    nav:false,
 	    autoplayHoverPause: false,
+	    touchDrag: false,
+	    mouseDrag: true,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
 	    responsive:{
